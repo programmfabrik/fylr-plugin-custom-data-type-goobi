bca78dba0f6e40ef37cb0ef3095094002f219bad
Tobias Fischer <tobias.fischer@gbv.de>
2026-04-14 11:50:40 +0000

    add custom expires_at config

--- src/updater/GoobiUpdater.js
+++ src/updater/GoobiUpdater.js
@@ -61,6 +61,15 @@ function isInTimeRange(currentHour, fromHour, toHour) {
   }
 }
 
+function getNewCustomExpiresAt() {
+  const newExpiresAt = new Date()
+  const customExpirationConfig = info?.config?.plugin?.['custom-data-type-goobi']?.config?.update_goobi?.custom_expires_days || 1
+
+  newExpiresAt.setDate(newExpiresAt.getDate() + customExpirationConfig);
+
+  return newExpiresAt.toISOString()
+}
+
 main = (payload) => {
 
   switch (payload.action) {
@@ -189,7 +198,11 @@ main = (payload) => {
               newCdata._standard = GoobiUtil.getStandardFromGoobiJSON(null, newCdata, newCdata, databaseLanguages);
               if (hasChanges(payload.objects[index].data, newCdata)) {
                 payload.objects[index].data = newCdata;
-              } else { }
+              } else {
+                payload.objects[index].data = originalCdata
+              }
+              // set expires at for the custom data object according to the plugin base config
+              payload.objects[index].data._expires_at = getNewCustomExpiresAt()
             }
           } else {
             console.error('No matching record found');
